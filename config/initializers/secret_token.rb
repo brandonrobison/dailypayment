a1adc458d1192b69d09a1acfce75da408c0a405a
Brandon Robison <robison.brandon@gmail.com>
2014-02-01 19:45:22 -0500

    Initial upload to github

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Dailypayment::Application.config.secret_key_base = 'db40e1501364048110c6d461c6467140183dbaedc1e00f3a7510cb312392c04bb050b53ecc8e05026d4857c8fd4d620f542f43f1d9e9021f3a6bc6f5a1a492cc'
